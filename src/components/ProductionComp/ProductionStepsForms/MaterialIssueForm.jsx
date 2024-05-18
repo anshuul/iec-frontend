8e447a26683aa3db2e1f710723bc7360b1bb21e6
Akshay200228 <akshayrs096@gmail.com>
2024-05-18 11:43:17 +0530

    working on material slip attachments

--- src/components/ProductionComp/ProductionStepsForms/MaterialIssueForm.jsx
+++ src/components/ProductionComp/ProductionStepsForms/MaterialIssueForm.jsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { FiArrowLeft, FiFile, FiPrinter, FiSave } from "react-icons/fi";
 import axios from "axios";
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 const MaterialIssueForm = () => {
   const router = useRouter();
@@ -25,6 +26,7 @@ const MaterialIssueForm = () => {
     "Default Quantity Issued"
   );
   const [selectedFile, setSelectedFile] = useState(null);
+
   useEffect(() => {
     handleCalculate();
   }, [diameter, length]);
@@ -57,25 +59,40 @@ const MaterialIssueForm = () => {
       localStorage.getItem("selectedCustomerPO")
     );
     try {
+      const formData = new FormData();
+      formData.append("poNo", selectedCustomerPODataForpoNo.poNo);
+      formData.append("materialSlipName", materialSlipName);
+      formData.append("itemDescription", itemDescription);
+      formData.append("materialGrade", materialGrade);
+      formData.append("lotNumber", lotNumber);
+      formData.append("size", size);
+      formData.append("diameterValue", diameter);
+      formData.append("diameterDimension", diameterDimension);
+      formData.append("lengthValue", length);
+      formData.append("lengthDimension", lengthDimension);
+      formData.append("quantityRequired", quantityRequired);
+      formData.append("quantityIssued", quantityIssued);
+
+      // Append poNo to the formData
+      formData.append("attachmentPoNo", selectedCustomerPODataForpoNo.poNo);
+
+      // Append the file if selected
+      if (selectedFile) {
+        formData.append("attachment", selectedFile);
+      }
+
       const response = await axios.post(
         "http://localhost:8000/api/materialissueslip/createMaterialIssueSlip",
+        formData,
         {
-          poNo: selectedCustomerPODataForpoNo.poNo,
-          materialSlipName,
-          itemDescription,
-          materialGrade,
-          lotNumber,
-          size: size,
-          diameterValue: diameter,
-          diameterDimension: diameterDimension,
-          lengthValue: length,
-          lengthDimension: lengthDimension,
-          quantityRequired,
-          quantityIssued,
+          headers: {
+            "Content-Type": "multipart/form-data", // Set content type to multipart form data
+          },
         }
       );
 
       console.log("response", response);
+      router.push("/production/material-issue-slip");
       // Optionally, handle success response, e.g., show a success message to the user
     } catch (error) {
       console.error("Error occurred while saving form data:", error);
@@ -159,23 +176,7 @@ const MaterialIssueForm = () => {
                 className="h-10 w-96 xl:w-[400px] px-6 text-[16px] text-black bg-white border-black border-2 rounded-lg border-opacity-50 outline-none focus:border-blue-500 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
               />
               <span className="text-[16px] text-black text-opacity-80 bg-white absolute left-4 top-1.5 px-1 transition duration-200 input-text">
-                Stud Description
-              </span>
-            </label>
-          </div>
-
-          {/* Nut Item Description */}
-          <div className="flex flex-col items-center gap-2 md:flex-row">
-          <label className="relative cursor-pointer App">
-              <input
-                type="text"
-                value={itemDescription}
-                onChange={(e) => setItemDescription(e.target.value)}
-                placeholder="Input"
-                className="h-10 w-96 xl:w-[400px] px-6 text-[16px] text-black bg-white border-black border-2 rounded-lg border-opacity-50 outline-none focus:border-blue-500 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
-              />
-              <span className="text-[16px] text-black text-opacity-80 bg-white absolute left-4 top-1.5 px-1 transition duration-200 input-text">
-                Nut Description
+                Item Description
               </span>
             </label>
           </div>
@@ -193,21 +194,7 @@ const MaterialIssueForm = () => {
               className="h-10 w-48 xl:w-[400px] px-6 text-[16px] text-black bg-white border-black border-2 rounded-lg border-opacity-50 outline-none focus:border-blue-500 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
             />
             <span className="text-[16px] text-black text-opacity-80 bg-white absolute left-4 top-1.5 px-1 transition duration-200 input-text">
-              Stud Grade
-            </span>
-          </label>
-          {/* Nut Grade */}
-          <label className="relative cursor-pointer">
-            <input
-              id="StudGrade"
-              type="text"
-              value={materialGrade}
-              onChange={(e) => setMaterialGrade(e.target.value)}
-              placeholder="Input"
-              className="h-10 w-48 xl:w-[400px] px-6 text-[16px] text-black bg-white border-black border-2 rounded-lg border-opacity-50 outline-none focus:border-blue-500 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
-            />
-            <span className="text-[16px] text-black text-opacity-80 bg-white absolute left-4 top-1.5 px-1 transition duration-200 input-text">
-              Nut Grade
+              Grade
             </span>
           </label>
         </div>
@@ -229,77 +216,7 @@ const MaterialIssueForm = () => {
           </label>
         </div>
 
-        <div className="flex flex-col items-start gap-2 my-4 md:items-center md:flex-row">
-          <label htmlFor="size" className="text-[16px] mr-4">
-            PO Size:
-          </label>
-          {/* Diameter */}
-          <label className="relative cursor-pointer App">
-            <input
-              id="sizeFirstPart"
-              type="text"
-              value={diameter}
-              onChange={(e) => setDiameter(e.target.value)}
-              placeholder="Input"
-              className="h-10 w-22 px-6 text-[16px] text-black bg-white border-black border-2 rounded-lg border-opacity-50 outline-none focus:border-blue-500 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
-            />
-            <span className="text-[16px] text-black text-opacity-80 bg-white absolute left-4 top-1.5 px-1 transition duration-200 input-text">
-              Diameter
-            </span>
-          </label>
-          <label
-            htmlFor="unit"
-            className="relative flex items-center cursor-pointer App"
-          >
-            <select
-              id="unit"
-              className="h-10 w-24 px-2 text-[16px] text-black bg-white border-black border-2 rounded-lg border-opacity-50 outline-none focus:border-blue-500 transition duration-200"
-              value={diameterDimension}
-              onChange={(e) => setDiameterDimension(e.target.value)}
-            // onChange={handleDiameterDimensionChange}
-            >
-              <option value="inch">Inch</option>
-              <option value="mm">MM</option>
-            </select>
-          </label>
-          {/* Unit */}
-          <label className="relative cursor-pointer App">
-            <input
-              id="sizeFirstPart"
-              type="text"
-              value={length}
-              onChange={(e) => setLength(e.target.value)}
-              placeholder="Input"
-              className="h-10 w-22 px-6 text-[16px] text-black bg-white border-black border-2 rounded-lg border-opacity-50 outline-none focus:border-blue-500 placeholder-gray-300 placeholder-opacity-0 transition duration-200"
-            />
-            <span className="text-[16px] text-black text-opacity-80 bg-white absolute left-4 top-1.5 px-1 transition duration-200 input-text">
-              Length
-            </span>
-          </label>
-          <label
-            htmlFor="unit"
-            className="relative flex items-center cursor-pointer App"
-          >
-            <select
-              id="unit"
-              className="h-10 w-24 px-2 text-[16px] text-black bg-white border-black border-2 rounded-lg border-opacity-50 outline-none focus:border-blue-500 transition duration-200"
-              value={lengthDimension}
-              onChange={(e) => setLengthDimension(e.target.value)}
-            // onChange={handleLengthDimensionChange}
-            >
-              <option value="inch">Inch</option>
-              <option value="mm">MM</option>
-            </select>
-          </label>
-
-          {/* Calculate button */}
-          <button
-            onClick={handleCalculate}
-            className="flex items-center px-4 py-2 ml-2 text-black bg-gray-300 rounded"
-          >
-            Calculation
-          </button>
-        </div>
+        
 
         {/* Cutting Size */}
         <div className="flex flex-col items-start gap-2 my-4 md:items-center md:flex-row">
@@ -410,7 +327,8 @@ const MaterialIssueForm = () => {
             type="file"
             id="attachment"
             className="hidden"
-            accept=".pdf"
+            name="attachment"
+            accept="application/pdf"
             onChange={handleFileSelection}
           />
           <button
@@ -420,7 +338,17 @@ const MaterialIssueForm = () => {
             Choose file
             <FiFile className="ml-2" />
           </button>
-          {selectedFile && <span className="ml-2">{selectedFile.name}</span>}
+          {selectedFile && (
+            <>
+              <span className="ml-2">{selectedFile.name}</span>
+              <button
+                onClick={() => setSelectedFile(null)}
+                className="flex items-center text-red-600 bg-none"
+              >
+                <IoIosCloseCircleOutline className="ml-2 text-2xl" />
+              </button>
+            </>
+          )}
         </div>
         <p className="ml-2 text-sm text-red-600">
           Only PDF files are allowed and only one file can be selected.
